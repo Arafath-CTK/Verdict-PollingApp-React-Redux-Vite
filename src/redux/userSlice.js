bfdbbe713649d2c6df58f74593bbb44181e6497e
pktdrarafath <pktdrarafath@gmail.com>
2024-06-01 13:22:32 +0000

    first commit

---
+++ src/redux/userSlice.js
@@ -0,0 +1,24 @@
+//createSlice function simplifies the process of creating
+//Redux actions and reducers.
+import { createSlice } from "@reduxjs/toolkit";
+import { act } from "react";
+
+const initialState = {
+  user: null,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      state.user = action.payload;
+    },
+    clearUser: (state) => {
+      state.user = null;
+    },
+  },
+});
+
+export const { setUser, clearUser } = userSlice.actions;
+export default userSlice.reducer;
